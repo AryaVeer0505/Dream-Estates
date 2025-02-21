47ea4458a3a01a98b940a8aa6c2bde444d48c297
AryaVeer0505 <aryaveerkanwar11458@gmail.com>
2025-02-21 14:18:02 +0530

    21-feb

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Login from "./Pages/Loginin";
 function App(){
   return(<>
-  
+  <Login/>
+     <h1>Aryaveer</h1>
   </>)
 }
 export default App
--- src/Pages/Loginin.jsx
+++ src/Pages/Loginin.jsx
@@ -1,6 +1,6 @@
 function Login(){
     return(<>
-    
+      <h1>Hello</h1>
     </>)
 }
 export default Login
